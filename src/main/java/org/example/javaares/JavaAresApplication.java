c25638f1c411f168152541c5a9c1ca2397ee9c52
Teemo-the-troll <mikulda@ka.cz>
2024-06-15 09:42:14 +0200

    Initial commit

---
+++ src/main/java/org/example/javaares/JavaAresApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaares/JavaAresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
