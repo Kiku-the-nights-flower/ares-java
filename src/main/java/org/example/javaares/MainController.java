7a54c59d0d1ef7021fa1c62ef515d2c36dd3a4e6
Teemo-the-troll <mikulda@ka.cz>
2024-06-15 13:16:58 +0200

    Ico validation
    - added validation for ico parameter
    - user is now required to use a valid ico, or older one that will be padded to represent the newer one

--- src/main/java/org/example/javaares/MainController.java
+++ src/main/java/org/example/javaares/MainController.java
@@ -21,16 +21,38 @@ public class MainController {
 
     /**
      * Main controller of the app
+     *
      * @param ico ico of the subject
      * @return String representation of the subject if found, "Not found" otherwise
-     * */
+     */
     @GetMapping("/")
     public String testing(@RequestParam String ico) {
+        if (!isIcoValid(ico))
+            return "Invalid ico";
+
         Optional<Subject> subject = service.getSubject(ico);
-        if (subject.isPresent()) {
+
+        if (subject.isPresent())
             return subject.get().toString();
+        return "Does not exist";
+    }
+
+
+    private boolean isIcoValid(String ico) {
+        StringBuilder icoBuilder = new StringBuilder(ico);
+        while (icoBuilder.length() < 8) {
+            icoBuilder.insert(0, "0");
         }
-        return "Not found";
+        ico = icoBuilder.toString();
+
+        for (int i = 0; i < ico.length(); i++) {
+            if (!Character.isDigit(ico.charAt(i))) {
+                return false;
+            }
+        }
+
+        int[] digits = ico.chars().map(Character::getNumericValue).toArray();
+        return (11 - (8 * digits[0] + 7 * digits[1] + 6 * digits[2] + 5 * digits[3] + 4 * digits[4] + 3 * digits[5] + 2 * digits[6]) % 11) % 10 == digits[7];
     }
 
 }
